eaad85078862cdff5fe2eca4ebc77ebb0033a203
Enyioma Osondu <o.enyioma@gmail.com>
2019-11-30 13:19:38 +0100

    ˢᵒᵐᵉᵇᵒᵈʸ ˢᵃᵛᵉ ᵐᵉ

--- config/celery.py
+++ config/celery.py
@@ -1,8 +1,10 @@
 import os
 from celery import Celery
 
+from django.conf import settings
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 celery_app = Celery('thumbnailer')
-celery_app.config_from_object('django.conf:settings', namespace='CELERY')
-celery_app.autodiscover_tasks()
+celery_app.config_from_object('django.conf:settings')
+celery_app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
